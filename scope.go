50436aea6acef58428243853db1ddd12be577947
Anis <contact.anisujjaman@gmail.com>
2025-03-14 01:12:27 +0600

    package scope

--- scope.go
+++ scope.go
@@ -1,28 +1,12 @@
 package main
 
-import "fmt"
+import "example.com/mathlib"
 
 var (
 	a = 20
 	b = 30
 )
 
-func add(x int, y int) {
-	z := x + y
-	fmt.Println(z)
-}
-
 func main() {
-	var p int = 100
-	var q = 200
-
-	add(p, q)
-	add(a, b)
-	add(p, a)
-
-	if a > 18 {
-		l := 10000
-		fmt.Println("This is a if block")
-		fmt.Println("This print", l, " Value")
-	}
+	mathlib.Add(a, b)
 }
