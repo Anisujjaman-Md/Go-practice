6ae98e98041d5ee63a92e817e9a1cf55bb5f3a31
Anis <contact.anisujjaman@gmail.com>
2025-03-15 01:11:10 +0600

    Different scope example

--- scope.go
+++ scope.go
@@ -1,12 +1,27 @@
 package main
 
-import "example.com/mathlib"
+import (
+	"fmt"
+
+	"example.com/mathlib"
+)
 
 var (
 	a = 20
 	b = 40
 )
 
+func addTwoNumber(x int, y int) {
+	res := x + y
+	printRes(res)
+}
+
 func main() {
 	mathlib.Add(a, b)
+	addTwoNumber(a, b)
+
+}
+
+func printRes(res int) {
+	fmt.Println("This is Final Result", res)
 }
